6937eaba12a4493fdc10e74d4ac6c8211aa18ee0
MuskanRehni <muskanrehni@gmail.com>
2025-01-19 00:25:43 +0530

    some changes

--- config/db.js
+++ config/db.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // Connect to MongoDB using the connection string from environment variables
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error(`Error: ${err.message}`);
@@ -14,3 +12,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
